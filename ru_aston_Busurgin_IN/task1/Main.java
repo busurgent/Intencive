c6da043183024906db111be3ef341cf2752db7e3
busurgent <bus-ilyukha@mail.ru>
2025-02-02 22:37:36 +0300

    first programm

---
+++ ru_aston_Busurgin_IN/task1/Main.java
@@ -0,0 +1,16 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        
+        int sum = calculator.add(10, 5);
+        int difference = calculator.subtract(10, 5);
+        int product = calculator.multiply(10, 5);
+        int quotient = calculator.divide(10, 5);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
+    }
+}
